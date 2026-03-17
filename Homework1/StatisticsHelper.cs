d8b3d55f37a94705bd6d412b789406750a303a08
Anastasiia <s25453@pjwstk.edu.pl>
2026-03-17 20:41:00 +0100

    Add constructor

--- Homework1/StatisticsHelper.cs
+++ Homework1/StatisticsHelper.cs
@@ -2,7 +2,15 @@ namespace Homework1;
 
 public class StatisticsHelper
 {
-    private IEnumerable<int> lista = new List<int>();
+    private List<int> lista;
+
+    public StatisticsHelper(List<int> lista)
+    {
+        this.lista = lista;
+    }
+
     Func<int, bool> parity = n => n%2 == 0;
+    
+    
     
 }
